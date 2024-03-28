e5300b6164e6d1dd405576e55e520ba993024030
Minhnguyenbao22 <minh.nb22012004@gmail.com>
2024-03-28 21:52:23 +0700

    28/03

--- components/HomeAdmin/HomeAdmin.js
+++ components/HomeAdmin/HomeAdmin.js
@@ -7,20 +7,41 @@ toggleMenuIcon.addEventListener("click", (e) => {
 });
 
 
+//Hiện form phân quyền
+const passWordAuthorityBtn = document.querySelector('.employee__form--passWordAuthority');
+const mainAuthority = document.querySelector('.mainAuthority');
+passWordAuthorityBtn.addEventListener('click', function() {
+    mainAuthority.style.display = 'block'; // Hiển thị bảng phân quyền khi nút được click
+});
+
+//Ẩn bảng phân quyền
+const closeButton = document.querySelector('.closeButton');
+closeButton.addEventListener('click', function() {
+    mainAuthority.style.display = 'none'; // Ẩn bảng phân quyền khi nút được click
+});
+
+
+
 document.addEventListener("DOMContentLoaded", function(){
-    var linkHome = document.getElementById('linkHome');
-    var linkPromotions = document.getElementById('linkPromotions');
-    var linkProduct = document.getElementById('linkProduct');
+    var linkHome = document.getElementById('link_Home');
+    var linkPromotions = document.getElementById('link_Promotions');
+    var linkProduct = document.getElementById('link_Product');
+    var linkStaff = document.getElementById('link_staff');
+    var linkClient = document.getElementById('link_client');
+
 
     var statisticsContent = document.getElementById('statisticsContent');
     var promotionsContent = document.getElementById('promotionsContent');
     var manageProduct = document.getElementById('manageProduct');
+    var staff = document.getElementById('staff');
+    var client = document.getElementById('client');
 
 
     statisticsContent.style.display = 'block';
     promotionsContent.style.display = 'none';
     manageProduct.style.display = 'none';
-
+    staff.style.display = 'none';
+    client.style.display = 'none';
     linkHome.classList.add('selected'); // Thêm lớp cho liên kết "Trang chủ"
 
 
@@ -30,10 +51,14 @@ document.addEventListener("DOMContentLoaded", function(){
          statisticsContent.style.display = 'block'; 
          promotionsContent.style.display = 'none'; 
          manageProduct.style.display = 'none';
+         staff.style.display = 'none';
+         client.style.display = 'none';
 
          linkHome.classList.add('selected'); // Thêm lớp cho liên kết "Trang chủ"
          linkPromotions.classList.remove('selected');
          linkProduct.classList.remove('selected');
+         linkStaff.classList.remove('selected');
+         linkClient.classList.remove('selected');
     });
 
 
@@ -42,10 +67,14 @@ document.addEventListener("DOMContentLoaded", function(){
       manageProduct.style.display = 'block';
       statisticsContent.style.display = 'none'; 
       promotionsContent.style.display = 'none'; 
+      staff.style.display = 'none';
+      client.style.display = 'none';
 
       linkHome.classList.remove('selected');
       linkPromotions.classList.remove('selected');
       linkProduct.classList.add('selected');
+      linkStaff.classList.remove('selected');
+      linkClient.classList.remove('selected');
     });
 
 
@@ -54,14 +83,47 @@ document.addEventListener("DOMContentLoaded", function(){
       statisticsContent.style.display = 'none'; 
       promotionsContent.style.display = 'block';
       manageProduct.style.display = 'none';
+      staff.style.display = 'none';
+      client.style.display = 'none';
 
       linkHome.classList.remove('selected');
       linkPromotions.classList.add('selected');
       linkProduct.classList.remove('selected');
+      linkStaff.classList.remove('selected');
+      linkClient.classList.remove('selected');
  });
+    linkStaff.addEventListener('click', function(event){
+      event.preventDefault();
+      statisticsContent.style.display = 'none'; 
+      promotionsContent.style.display = 'none';
+      manageProduct.style.display = 'none';
+      staff.style.display = 'block';
+      client.style.display = 'none';
 
+
+      linkHome.classList.remove('selected');
+      linkPromotions.classList.remove('selected');
+      linkProduct.classList.remove('selected');
+      linkStaff.classList.add('selected');
+      linkClient.classList.remove('selected');
+
+     });
     
+    linkClient.addEventListener('click', function(event){
+      event.preventDefault();
+      statisticsContent.style.display = 'none'; 
+      promotionsContent.style.display = 'none';
+      manageProduct.style.display = 'none';
+      staff.style.display = 'none';
+      client.style.display = 'block';
 
+
+      linkHome.classList.remove('selected');
+      linkPromotions.classList.remove('selected');
+      linkProduct.classList.remove('selected');
+      linkStaff.classList.remove('selected');
+      linkClient.classList.add('selected');
+    })
 });
 
 
